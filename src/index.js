3252a0d3df00cf2d87b08c9ab575985c89b1040d
LimitToInfinity <21stLimitless@gmail.com>
2019-10-16 18:47:27 -0600

    First commit.

--- src/index.js
+++ src/index.js
@@ -1,94 +1,124 @@
-const addBtn = document.querySelector('#new-toy-btn')
-const toyForm = document.querySelector('.container')
-const toyCollection = document.querySelector("#toy-collection")
 const toyURL = "http://localhost:3000/toys"
+
+const addBtn = document.querySelector('#new-toy-btn')
+const toyFormContainer = document.querySelector('.container')
+const toyForm = document.querySelector(".add-toy-form")
+const toyCollection = document.querySelector("#toy-collection")
+
 let addToy = false
 let allToys = null
 
-document.addEventListener("DOMContentLoaded", () =>{
-  getToys()
-})
+getToys()
 
 function getToys() {
   fetch(toyURL)
-  .then(response => response.json())
+  .then(parseJSON)
   .then(showToys)
+}
+
+addBtn.addEventListener('click', displayToy)
+toyFormContainer.addEventListener('submit', newToy)
+
+function displayToy() {
+  addToy = !addToy
+  if (addToy) {
+    toyFormContainer.style.display = 'block'
+  } else {
+    toyFormContainer.style.display = 'none'
+  }
+}
+
+function newToy(event) {
+  event.preventDefault()
+
+  // const toyName = document.querySelectorAll(".input-text")[0]["value"]
+  // const toyImg = document.querySelectorAll(".input-text")[1]["value"]
+
+  const formData = new FormData(toyForm);
+
+  const name = formData.get("name");
+  const image = formData.get("image");
+  const likes = 0
+  
+  const toyBody = {
+    name,
+    image,
+    likes
+  }
+
+  fetchCall(toyURL, "POST", toyBody)
+  .then(parseJSON)
+  .then(addToAllToys)
+  .then(createToyCard)
+}
+
+function addToAllToys(toy) {
+  return toy
 }
 
 function showToys(toys) {
   allToys = toys
- toys.forEach(toy =>{
-   const card = document.createElement('div')
-   card.className = 'card'
-   card.innerHTML = `
-      <h2>${toy.name}</h2>
-      <img src=${toy.image} class="toy-avatar" />
-      <p>${toy.likes} Likes</p>
-      <button class="like-btn">Like <3</button>
-   `
-   toyCollection.appendChild(card)
- })
- likeButton()
+
+  toys.forEach(createToyCard)
 }
 
-addBtn.addEventListener('click', () => {
-  addToy = !addToy
-  if (addToy) {
-    toyForm.style.display = 'block'
-  } else {
-    toyForm.style.display = 'none'
-  }
-})
+function createToyCard(toy) {
 
-toyForm.addEventListener('submit', () => {
-  event.preventDefault()
-  const toyName = document.querySelectorAll(".input-text")[0]["value"]
-  const toyImg = document.querySelectorAll(".input-text")[1]["value"]
-  fetch(toyURL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json"
-    },
-    body: JSON.stringify({
-      "name": toyName,
-      "image": toyImg,
-      "likes": 0
-    })
-  }).then(response => response.json())
-})
+  const card = document.createElement('div')
+  card.className = 'card'
 
-function likeButton() {
-  const allLikeButtons = document.querySelectorAll(".like-btn")
-  for (likeButton of allLikeButtons) {
-    likeButton.addEventListener("click", (e) => {
-      increaseLikes(e)
-    })
-  }
+  console.log(toy)
+
+  card.innerHTML = `
+  <h2>${toy.name}</h2>
+  <img src=${toy.image} class="toy-avatar" />
+  <p>${toy.likes} Likes</p>
+  `  
+  const likeButton = document.createElement("button");
+  likeButton.classList.add("like-btn")
+  likeButton.innerText = "Like <3"
+
+  card.append(likeButton);
+  toyCollection.appendChild(card)
+
+  likeButton.addEventListener("click", increaseLikes)
 }
 
-function increaseLikes(e) {
-  const likeElement =e.target.parentNode.querySelector('p')
+function increaseLikes(event) {
+  const likeElement = event.target.parentNode.querySelector('p')
   const likes = likeElement.innerText
   const likesArray = likes.split(" ")
   const newLikes = parseInt(likesArray[0]) + 1
+
   likeElement.innerText = `${newLikes} Likes`
-  dbIncrease(e, newLikes)
+  
+  dbIncrease(event, newLikes)
 }
 
-function dbIncrease(e, newLikes){
-  const toyName = e.target.parentNode.querySelector('h2').innerText
+function dbIncrease(event, newLikes){
+  const toyName = event.target.parentNode.querySelector('h2').innerText
   let toyObj = allToys.filter(toy => toy.name === toyName)
   toyObj = toyObj[0]
-  fetch(`${toyURL}/${toyObj.id}`, {
-    method: "PATCH",
-    headers: {
+
+  const thisToyURL = `${toyURL}/${toyObj.id}`
+  const toyBody = { "likes": newLikes }
+  
+  fetchCall(thisToyURL, "PATCH", toyBody)
+}
+
+function fetchCall(url, method, body) {
+  return fetch(url, 
+  {
+    method,
+    headers:
+    {
       "Content-Type": "application/json",
       "Accept": "application/json"
     },
-    body: JSON.stringify({
-      "likes": newLikes
-    })
+    body: JSON.stringify(body)
   })
-  .then(response => response.json())
+}
+
+function parseJSON(response) {
+  return response.json()
 }
